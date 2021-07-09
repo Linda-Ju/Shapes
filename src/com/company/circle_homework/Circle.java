a21d75faad2814b5816212d6a01f10b7ab0a2831
Linda-Ju <linda.junkina@gmail.com>
2021-07-09 21:28:57 +0300

    Circle(Area, Perimeter)

---
+++ src/com/company/circle_homework/Circle.java
@@ -0,0 +1,24 @@
+package com.company.circle_homework;
+
+public class Circle {
+//    int radius;
+//
+//    public Circle() {
+//    }
+//
+//    public Circle(int radius) {
+//        this.radius = radius;
+//    }
+//
+//    public int getRadius() {
+//        return radius;
+//    }
+//
+//    public void setRadius(int radius) {
+//        this.radius = radius;
+//    }
+//
+//    public float Area(){
+//        return Math.PI * this.radius
+//    }
+}
---
+++ src/com/company/circle_homework/CircleDemo.java
@@ -0,0 +1,16 @@
+package com.company.circle_homework;
+
+import java.util.Scanner;
+
+public class CircleDemo {
+//    public static void main(String[] args) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a radius: ");
+//
+//        Circle circle = new Circle(scanner.nextInt());
+//
+//
+//
+//    }
+}
