8f4867dedd5f1b39921695d05c135e4a3fb5dc87
Linda-Ju <linda.junkina@gmail.com>
2021-07-12 11:43:04 +0300

    Circle(Area, Circumference)

--- src/com/company/circle_homework/Circle.java
+++ src/com/company/circle_homework/Circle.java
@@ -1,24 +1,28 @@
 package com.company.circle_homework;
 
 public class Circle {
-//    int radius;
-//
-//    public Circle() {
-//    }
-//
-//    public Circle(int radius) {
-//        this.radius = radius;
-//    }
-//
-//    public int getRadius() {
-//        return radius;
-//    }
-//
-//    public void setRadius(int radius) {
-//        this.radius = radius;
-//    }
-//
-//    public float Area(){
-//        return Math.PI * this.radius
-//    }
+    int radius;
+
+    public Circle() {
+    }
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public float area() {
+        return (float) (Math.PI * (this.radius * this.radius));
+    }
+
+    public float circumference() {
+        return (float) (Math.PI * 2 * this.radius);
+    }
 }
--- src/com/company/circle_homework/CircleDemo.java
+++ src/com/company/circle_homework/CircleDemo.java
@@ -3,14 +3,17 @@ package com.company.circle_homework;
 import java.util.Scanner;
 
 public class CircleDemo {
-//    public static void main(String[] args) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter a radius: ");
-//
-//        Circle circle = new Circle(scanner.nextInt());
-//
-//
-//
-//    }
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a radius: ");
+
+        Circle circle = new Circle(scanner.nextInt());
+
+        System.out.println(circle.area());
+        System.out.println(circle.circumference());
+
+
+
+    }
 }
